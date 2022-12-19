00457a78f1b5dd4d697cfd6410b0d0691f87e7d1
hamzauguz <arastiranhamza@gmail.com>
2022-12-19 08:48:21 +0300

    add screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Routes, Route, BrowserRouter, useLocation } from "react-router-dom";
 import Header from "./components/header";
+import Contact from "./screens/Contact";
 import Home from "./screens/Home";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
       <div>
         <Routes>
           <Route exact path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </BrowserRouter>
